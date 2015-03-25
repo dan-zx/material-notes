e6cafe38ff3363eff77acdef2b77a655563e8c73
Suraj Patil <sapatil@live.com>
2015-03-25 11:03:13 +0530

    added search & share

--- app/src/main/java/com/materialnotes/widget/NotesAdapter.java
+++ app/src/main/java/com/materialnotes/widget/NotesAdapter.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import com.materialnotes.R;
 import com.materialnotes.data.Note;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import java.text.DateFormat;
 import java.util.List;
@@ -18,7 +20,7 @@ import java.util.List;
  * @author Daniel Pedraza Arcega
  * @see <a href="http://bit.ly/1vZt3ny">Building Layouts with an Adapter</a>
  */
-public class NotesAdapter extends BaseAdapter {
+public class NotesAdapter extends BaseAdapter implements Filterable {
 
     /** Wrapper para notas. Util para cambiar el fondo de los item seleccionados. */
     public static class NoteViewWrapper {
@@ -47,6 +49,7 @@ public class NotesAdapter extends BaseAdapter {
     private static final DateFormat DATETIME_FORMAT = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
 
     private final List<NoteViewWrapper> data;
+    private List<NoteViewWrapper> filtered_data;
 
     /**
      * Constructor.
@@ -55,12 +58,13 @@ public class NotesAdapter extends BaseAdapter {
      */
     public NotesAdapter(List<NoteViewWrapper> data) {
         this.data = data;
+        this.filtered_data = data;
     }
 
     /** @return cuantos datos hay en la lista de notas. */
     @Override
     public int getCount() {
-        return data.size();
+        return filtered_data.size();
     }
 
     /**
@@ -69,7 +73,7 @@ public class NotesAdapter extends BaseAdapter {
      */
     @Override
     public NoteViewWrapper getItem(int position) {
-        return data.get(position);
+        return filtered_data.get(position);
     }
 
     /**
@@ -100,7 +104,7 @@ public class NotesAdapter extends BaseAdapter {
             convertView.setTag(holder);
         } else holder = (ViewHolder) convertView.getTag(); // ya existe, solo es reciclarlo
         // Inicializa la vista con los datos de la nota
-        NoteViewWrapper noteViewWrapper = data.get(position);
+        NoteViewWrapper noteViewWrapper = filtered_data.get(position);
         holder.noteIdText.setText(String.valueOf(noteViewWrapper.note.getId()));
         holder.noteTitleText.setText(noteViewWrapper.note.getTitle());
         // Corta la cadena a 80 caracteres y le agrega "..."
@@ -136,4 +140,45 @@ public class NotesAdapter extends BaseAdapter {
             noteDateText = (TextView) parent.findViewById(R.id.note_date);
         }
     }
+
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter(){
+
+            @Override
+            protected FilterResults performFiltering(CharSequence query) {
+                FilterResults results = new FilterResults();
+                ArrayList<NoteViewWrapper> filtered = new ArrayList<>();
+                query = query.toString().toLowerCase();
+                if(query!=null && query.length()>0){
+                    for(int i=0;i<data.size();i++){
+                        String dataName = data.get(i).getNote().getContent().toLowerCase();
+                        String title = data.get(i).getNote().getTitle().toLowerCase();
+                        if (dataName.contains(query) or title.contain(query)){
+                            filtered.add(data.get(i));
+                        }
+
+                    }
+                    results.count = filtered.size();
+                    results.values = filtered;
+
+                }else{
+                    results.count = data.size();
+                    results.values = data;
+                }
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                filtered_data =  (ArrayList<NoteViewWrapper>) results.values;
+                NotesAdapter.this.notifyDataSetChanged();
+            }
+        };
+
+        return filter;
+    }
+
 }
